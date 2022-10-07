066a66c991f0f2b85e5b67caa1c346ec617d6cdb
Ilham cahya Ramadhan <88922875+realiyams@users.noreply.github.com>
2022-10-07 09:47:53 +0700

    I've completed the project

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+
 require('dotenv').config()
 
 var app = express();
@@ -11,8 +13,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post("/api/fileanalyse", multer().single('upfile'), (req, res) => {
+  if (req.file !== undefined)
+    res.json({
+      name : req.file.originalname,
+      type : req.file.mimetype,
+      size : req.file.size
+    })
+  else 
+    res.send("No file uploaded")
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
